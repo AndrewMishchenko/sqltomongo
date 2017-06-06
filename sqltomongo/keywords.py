e7c5f96fa5c7a88fcd9b5030d83cf1b14e4bbac0
AndrewMishchenko <a.voskresenskyi@gmail.com>
2017-06-06 09:36:42 +0300

    Aded sqltomongo dir and keywords.py

---
+++ sqltomongo/keywords.py
@@ -0,0 +1,12 @@
+KEYWORDS = {
+    'DML': ['SELECT'],  # Data Manipulation Language
+    'KEYWORD': ['WHERE', 'FROM', 'BY', 'ORDER'],
+    'LOGIC': ['AND', 'OR'],
+    'SKIP': 'SKIP',
+    'LIMIT': 'LIMIT',
+    'COMPARISONS': ['>', '>=', '<', '<=', '<>', '='],
+    'ORDER': ['DESC', 'ASC'],
+    'ASTERISK': '*',
+    'COMA': ',',
+    'DOT': '.'
+}
