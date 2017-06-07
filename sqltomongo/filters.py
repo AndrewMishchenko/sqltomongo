ad4cb7d049003496a7c1d9cde4ed926e4dae3f2f
AndrewMishchenko <a.voskresenskyi@gmail.com>
2017-06-07 16:54:23 +0300

    Added checking if isinstance in order_filter_find

--- sqltomongo/filters.py
+++ sqltomongo/filters.py
@@ -2,6 +2,7 @@ from pymongo import ASCENDING, DESCENDING
 
 from sqltomongo.keywords import KEYWORDS
 
+
 def coma_filter(list_with_coma):
     if not isinstance(list_with_coma, list):
         raise ValueError("The type of list_with_coma must be a list")
@@ -33,13 +34,13 @@ def unwind_filter(projection):
         unw = list()
         for obj in projection:
             if obj.endswith('.*'):
-                obj = (obj.replace('.*', '')) #.split('.')
+                obj = (obj.replace('.*', ''))  # .split('.')
                 unw.append(obj)
             else:
                 pass
         return unw
 
-a = ['restaurant_id', 'desc,', 'borough', 'asc']
+
 def order_filter(order_list):
     if not isinstance(order_list, list):
         raise ValueError("The type of order must be a list")
@@ -58,19 +59,23 @@ def order_filter(order_list):
             filtered[key] = obj
         return {'$sort': filtered}
 
+
 def order_filter_find(order_list):
     """
     Get dirty format of ordering expression and zip it into normal.
     :param o_list: list of tuples in format [('value_to_order', 1/-1)]
     :return: filtered ordering expression
     """
-    filtered_list = list()
-    for obj in coma_filter(order_list):
-        if obj.upper() == 'ASC':
-            obj = ASCENDING
-        elif obj.upper() == 'DESC':
-            obj = DESCENDING
-        filtered_list.append(obj)
-    filtered_list = iter(filtered_list)
-    filtered_list = [i for i in zip(filtered_list, filtered_list)]
-    return filtered_list
+    if not isinstance(order_list, list):
+        raise ValueError("The type of order must be a list")
+    else:
+        filtered_list = list()
+        for obj in coma_filter(order_list):
+            if obj.upper() == 'ASC':
+                obj = ASCENDING
+            elif obj.upper() == 'DESC':
+                obj = DESCENDING
+            filtered_list.append(obj)
+        filtered_list = iter(filtered_list)
+        filtered_list = [i for i in zip(filtered_list, filtered_list)]
+        return filtered_list
