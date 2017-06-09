a0d9ba2cff1e9013abe4bd60eeeb58454703da08
AndrewMishchenko <a.voskresenskyi@gmail.com>
2017-06-09 12:41:13 +0300

    Changed the logic of work DatabaseConnection class

--- sqltomongo/database.py
+++ sqltomongo/database.py
@@ -1,23 +1,79 @@
+import sys
+
 from pymongo import MongoClient
+from pymongo.database import Database
+from pymongo.errors import ServerSelectionTimeoutError, OperationFailure
 
-from sqltomongo.exceptions import SqmongoConnectionError
 
+class DatabaseConnection(object):
+    def __init__(self, database=None, host='localhost', port=27017):
+        """Establish the connection with the mongo backend"""
 
-class DatabaseConnect(object):
-    """Initializes the connection to the database."""
-
-    def __init__(self, db=None):
-        """
-        :param db: The name of database
-        """
+        self.timeout = 1
+        self.host = str(host)
+        if not isinstance(port, int):
+            raise ValueError('Entered port is "{}"! Must be integer type'.
+                             format(port))
+        self.port = port
+        self.client = MongoClient(host=self.host,
+                                  port=self.port,
+                                  serverSelectionTimeoutMS=self.timeout)
         try:
-            if db is None:
-                raise SqmongoConnectionError(db)
-        except SqmongoConnectionError:
-            pass
-        else:
-            self.db = str(db)
-            self.client = MongoClient()
+            if database is not None:
+                if database in self.client.database_names():
+                    self.database = Database(self.client, database)
+                    if self.database.command("serverStatus")['ok'] == 1.0:
+                        print('connecting to: mongodb://{}:{}'.format(
+                            self.host, self.port))
+                        print('database - "{}"'.format(self.database.name))
+                    else:
+                        pass
 
-    def _connection(self):
-        return self.client[self.db]
+                else:
+                    raise ValueError
+            else:
+                self.database = database
+
+        except ServerSelectionTimeoutError as err:
+            sys.exit('Error! {}'.format(err))
+        except ValueError:
+            print('"{}" is not in database names! Please check the name of '
+                  'database!'.format(database))
+
+    def use_db(self, database=None):
+        try:
+            if database in self.client.database_names():
+                self.database = Database(self.client, database)
+                print('switched to db {}'.format(self.database.name))
+            else:
+                raise ValueError
+        except ServerSelectionTimeoutError as err:
+            sys.exit('Error! {}'.format(err))
+        except ValueError:
+            print('"{}" is not in database names! Please check the name of '
+                  'database!'.format(database))
+
+    def authenticate(self, user=None, password=None):
+        if user is not None and password is not None:
+            try:
+                if user in [user['user'] for user in (
+                        self.database.command('usersInfo')
+                )['users']]:
+                    try:
+                        if self.database.authenticate(user, password):
+                            self.auth = self.database.authenticate(user,
+                                                                   password)
+                            print('1')
+                        else:
+                            raise OperationFailure
+                    except OperationFailure:
+                        print('Error: Authentication failed.')
+                        print('0')
+                else:
+                    print('"{}" user is not in "{}" database users!'.format(
+                        user, self.database.name))
+            except AttributeError:
+                print('At first you must connect to the database with use '
+                      'method!')
+        else:
+            print('Please check entered user and password!')
